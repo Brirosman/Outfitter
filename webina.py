2ee2bd1d85ec3f45d7cdfe569b7ceb42f1c82a00
Brirosman <brirosman10@gmail.com>
2024-10-22 15:28:58 -0300

    Fastapi 2
    Sigo intentando la conexion

--- webina.py
+++ webina.py
@@ -2,7 +2,7 @@ from typing import Union
 from funka import *
 from index import *
 from links import *
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
 
@@ -12,7 +12,17 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
+@app.get("/upload/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+@app.post("/upload")
+async def upload_image(file: UploadFile = File(...)):
+    image_path = f"./uploads/{file.filename}"  
+    with open(image_path, "wb") as image:
+        content = await file.read()
+        image.write(content)
+
+    tags, texts, colors = analyze_image(image_path)
+
+    return {"tags": tags, "texts": texts, "colors": colors}
 
