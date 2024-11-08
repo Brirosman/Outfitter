17d925d80817bc3d50d7369b141dce23bca6d490
AndyLevinson <151685465+AndyLevinson@users.noreply.github.com>
2024-11-08 11:14:31 -0300

    Update webina.py

--- webina.py
+++ webina.py
@@ -24,22 +24,22 @@ async def read_root():
 
 
 
-app = FastAPI()
+# app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+# app.mount("/static", StaticFiles(directory="static"), name="static")
 
-templates = Jinja2Templates(directory="templates")
+# templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/inicio", response_class=HTMLResponse)
-async def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+# @app.get("/inicio", response_class=HTMLResponse)
+# async def home(request: Request):
+#     return templates.TemplateResponse("index.html", {"request": request})
 
-@app.get("/links.html", response_class=HTMLResponse)
-async def home(request: Request):
-    return templates.TemplateResponse("links.html", {"request": request})
+# @app.get("/links.html", response_class=HTMLResponse)
+# async def home(request: Request):
+#     return templates.TemplateResponse("links.html", {"request": request})
 
-# @app.post("/analizar/")
+# # @app.post("/analizar/")
 # async def analyze_image (file: UploadFile = File(...)):
 #     contents = await file.read()
 #     with open("funka.py", "wb") as f:
